2288db26044263db0263f0ea71ec04df8b19bbee
ravi <ravi.20930@gmail.com>
2023-06-23 23:01:01 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,92 @@
+const cron = require('node-cron');
+const { loadToken, gmail } = require("./auth");
+
+async function replyToEmail(emailId, subject, body) {
+    try {
+        // const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+
+        const email = await gmail.users.messages.get({
+            userId: 'me',
+            id: emailId,
+        });
+
+        const originalSender = email.data.payload.headers.find(
+            (header) => header.name === 'From'
+        ).value;
+
+        await gmail.users.messages.send({
+            userId: 'me',
+            requestBody: {
+                raw: Buffer.from(
+                    `From: "me"\nTo: ${originalSender}\nSubject: ${subject}\n\n${body}`
+                ).toString('base64'),
+                threadId: emailId,
+            },
+        });
+        await gmail.users.messages.modify({
+            userId: 'me',
+            id: emailId,
+            requestBody: {
+                addLabelIds: ['INBOX'],
+                removeLabelIds: ['UNREAD'],
+            },
+        });
+        console.log('Reply sent.');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function listAndReplyToEmails() {
+    try {
+        // const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+        // maxResults is number of mails you want to send auto replies to in every call.
+        const res = await gmail.users.messages.list({
+            userId: 'me',
+            labelIds: ['UNREAD', 'INBOX'],
+            maxResults: 1,
+        });
+
+        const emails = res.data.messages;
+        if (emails && emails.length) {
+            console.log('Recent emails:');
+            for (const email of emails) {
+                const res = await gmail.users.messages.get({
+                    userId: 'me',
+                    id: email.id,
+                });
+
+                const subject = res.data.payload.headers.find(
+                    (header) => header.name === 'Subject'
+                ).value;
+
+                const sender = res.data.payload.headers.find(
+                    (header) => header.name === 'From'
+                ).value;
+
+                console.log('- Sender:', sender); // Log the sender's email
+                console.log('- Subject:', subject); // Log the subject of each email
+
+                const body = 'Thank you for your email. Your message has been received and will be processed shortly.';
+                await replyToEmail(email.id, subject, body);
+            }
+        } else {
+            console.log('No new emails.');
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// (async () => {
+//     await loadToken();
+//     await listAndReplyToEmails();
+// })();
+
+// cron job to monitor mails every 5 minutes
+console.log("checking for mails every 5 minutes.");
+cron.schedule('*/5 * * * *', async () => {
+    console.log('Checking for new emails...');
+    await loadToken();
+    await listAndReplyToEmails();
+});
---
+++ auth.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const { google } = require('googleapis');
+const readline = require('readline');
+const TOKEN_PATH = 'token.json';
+const credentials = JSON.parse(fs.readFileSync('credentials.json'));
+const { client_secret, client_id, redirect_uris } = credentials.installed;
+
+const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+);
+const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
+
+const authorize = async () => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+        prompt: 'consent',
+    });
+
+    console.log('Authorize this app by visiting this URL:', authUrl);
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    const code = await new Promise((resolve) => {
+        rl.question('Enter the authorization code here: ', (code) => {
+            rl.close();
+            resolve(code);
+        });
+    });
+
+    const { tokens } = await oAuth2Client.getToken(code);
+    oAuth2Client.setCredentials(tokens);
+    fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+    console.log('Token stored to', TOKEN_PATH);
+}
+
+exports.loadToken = async () => {
+    try {
+        const token = fs.readFileSync(TOKEN_PATH);
+        oAuth2Client.setCredentials(JSON.parse(token));
+    } catch (err) {
+        console.error(err);
+        await authorize();
+    }
+}
+
+exports.gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
